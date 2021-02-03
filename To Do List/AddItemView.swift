68c41f15aaf1b3c60bdecf771ddf0348c1d0d459
Paige Bowman <paigeelisabethbowman@gmail.com>
2021-02-03 10:59:41 -0600

    Added navigation bar title.

--- To Do List/AddItemView.swift
+++ To Do List/AddItemView.swift
@@ -19,11 +19,22 @@ struct AddItemView: View {
         NavigationView {
             Form {
                 Picker("Priority", selection: $priority) {
+                    TextField("Description", text: $description)
+                    DatePicker("Due Date", selection: $dueDate, displayedComponents: .date)
                     ForEach(Self.priorities, id: \.self) { priority in
                         Text(priority)
                     }
                 }
             }
+            navigationBarTitle("Add New To-Do Item")
+                .navigationBarItems(trailing: Button("Save") {
+                    if priority.count > 0 && description.count > 0 {
+                        let item = ToDoItem(id: UUID(), priority: priority,
+                                            description: description, dueDate: dueDate)
+                        toDoList.items.append(item)
+                        presentationMode.wrappedValue.dismiss()
+                    }
+                })
         }
     }
 }
